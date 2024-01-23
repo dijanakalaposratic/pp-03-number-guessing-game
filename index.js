ed7583dd367ea5cbecf7d264c7a91224b741c07d
dijanakalaposratic <dijanakalaposratic@gmail.com>
2024-01-24 00:10:01 +0100

    added content to index.js

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ let guess;
 let running = true;
 
 while (running) {
-    guess = prompt(`Guess a number between ${minNum} and ${maxNum}`);
-    guess = Number(guess);
+	guess = prompt(`Guess a number between ${minNum} and ${maxNum}`);
+	guess = Number(guess);
 
-    if (Number.isNaN(guess)) {
-        alert('Please enter a valid number');
-    } else if (guess > maxNum || guess < minNum) {
-        alert('Please guess a number between ' + minNum + ' and ' + maxNum);
-    } else {
-        attempts++;
-        if (guess < answer) {
-            alert('Too low! Try again.');
-        } else if (guess > answer) {
-            alert('Too high! Try again.');
-        } else {
-            alert('You got it! the answer is ${answer}. It took you ${attempts} attempts.');
-            running = false;
-        }
-    }
+	while (isNaN(guess) || guess < minNum || guess > maxNum) {
+		guess = prompt(`Please enter a valid number between ${minNum} and ${maxNum}`);
+		guess = Number(guess);
+	}
+
+	attempts++;
+
+	if (guess < answer) {
+		alert("Too low! Try again.");
+	} else if (guess > answer) {
+		alert("Too high! Try again.");
+	} else {
+		alert(`You got it! The answer is ${answer}. It took you ${attempts} attempts.`);
+		running = false;
+	}
+}
